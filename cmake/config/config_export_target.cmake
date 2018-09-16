73a624d4a5c2856248d376c78cf0e37a353d2459
Alexis Constantin Link <aconstlink@gmail.com>
2018-09-16 23:25:06 +0200

    - macro for clearing configure script

--- cmake/config/config_export_target.cmake
+++ cmake/config/config_export_target.cmake
@@ -3,6 +3,12 @@
 macro( so_export name )
 
   #message( ${name} )
-  export( TARGETS ${name} APPEND FILE ${CMAKE_BINARY_DIR}/snakeoil-targets2.cmake )
+  export( TARGETS ${name} APPEND FILE ${CMAKE_BINARY_DIR}/snakeoil-targets.cmake )
+
+endmacro()
+
+macro( so_export_clear )
+
+file( REMOVE ${CMAKE_BINARY_DIR}/snakeoil-targets.cmake )
 
 endmacro()

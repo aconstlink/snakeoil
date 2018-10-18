3f7b8bc5e6e7c68f4cc80afd348670ee904135fc
Alexis Constantin Link <aconstlink@gmail.com>
2018-10-18 20:41:34 +0200

    - cmake variable name fixes

--- cmake/config/config_export_target.cmake
+++ cmake/config/config_export_target.cmake
@@ -3,12 +3,12 @@
 macro( so_export name )
 
   #message( ${name} )
-  export( TARGETS ${name} APPEND FILE ${CMAKE_BINARY_DIR}/snakeoil-targets.cmake )
+  export( TARGETS ${name} APPEND FILE ${SNAKEOIL_BINARY_DIR}/snakeoil-targets.cmake )
 
 endmacro()
 
 macro( so_export_clear )
 
-file( REMOVE ${CMAKE_BINARY_DIR}/snakeoil-targets.cmake )
+file( REMOVE ${SNAKEOIL_BINARY_DIR}/snakeoil-targets.cmake )
 
 endmacro()

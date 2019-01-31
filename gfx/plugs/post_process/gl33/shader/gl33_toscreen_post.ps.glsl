d7dff2b2ec89ad70b7d0db392b0bdd8bbe2446cd
Alexis Constantin Link <aconstlink@gmail.com>
2019-02-01 00:32:46 +0100

    - shader fix

--- gfx/plugs/post_process/gl33/shader/gl33_toscreen_post.ps.glsl
+++ gfx/plugs/post_process/gl33/shader/gl33_toscreen_post.ps.glsl
@@ -12,18 +12,6 @@ in vertex_data
 
 void main()
 {
-    //vec2 v = psi.tx * vec2(2.0) - vec2(1.0) ;
-    //float l = length( v * vec2(0.3,0.9) ) ;
-    //l = pow( l, 2 ) ;
-    
-    //float vignette = 1.0 - pow( l, 1 ) ;
-
     vec4 color_image = texture( u_smp_color, psi.tx );
-
-    out_color = color_image * vec4(0.9,0.9,0.9,1.0) ;
-    //out_color *= vignette ;
- 
-    //out_color = vec4( 1.0,1.0,1.0,1.0 ) ;
-
-    //out_color = vec4(1.0, 0.0, 0.0, 1.0 ) ;
+    out_color = color_image ;    
 }

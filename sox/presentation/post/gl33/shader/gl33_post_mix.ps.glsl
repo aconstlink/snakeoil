25e40f25f3fa48b1a25b9cd4ca261f47fd3eeac8
aconstlink <aconstlink@gmail.com>
2020-02-23 00:10:11 +0100

    - changing the transition mix shader where we only need two value instead of three for mixing scenes

--- sox/presentation/post/gl33/shader/gl33_post_mix.ps.glsl
+++ sox/presentation/post/gl33/shader/gl33_post_mix.ps.glsl
@@ -21,9 +21,10 @@ void main()
     vec4 tex_x = texture( u_smp_color_x, psi.tx );
     vec4 tex_m = texture( u_smp_mask, psi.tx );
 
-    vec4 color_a = mix( tex_x, tex_a, tex_m.r ) ;
-    vec4 color_b = mix( tex_x, tex_b, tex_m.b ) ;
-        
-    out_color = mix( color_a, color_b, tex_x.a) ;    
+    vec4 color_a = mix( tex_a, tex_b, tex_m.r ) ;
+    vec4 color_b = mix( tex_x, color_a, tex_m.g ) ;
+      
+    out_color = color_b ;
+    //out_color = mix( color_a, color_b, tex_x.a) ;    
     //out_color = vec4(.8,0.8,.8,1.0);
 }
